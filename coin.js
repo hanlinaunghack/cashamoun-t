6e86e65b64c2906ce1828c9fded707985acb4469
Han Lin Aung <hanlinaunghack@gmail.com>
2018-10-18 10:10:34 -0700

    fixed some issues

--- coin.js
+++ coin.js
@@ -1,7 +1,7 @@
 class Cash {
   constructor(num) {
     this.num = num;
-    this.double;
+    this.double = 0;
   }
   totalInPennies() {
     let x = Number(this.num) ? this.num.toFixec(2) : NaN;
@@ -24,7 +24,7 @@ class Cash {
   quantityOfEachDenomination() {
     let x = this.num;
     let obj = {};
-    let factor = [100, 50, 20, 10, 5, 1, 0.5, 0.25, 0.01];
+    let factor = [100, 50, 20, 10, 5, 1, 0.5, 0.25, 0.1, 0.05, 0.01];
 
     for (var i = 0; i < factor.length; i++) {
       if (Math.floor(x / factor[i]) > 0) {
@@ -48,10 +48,7 @@ const obj = {
   nickels: 1,
   pennies: 3
 };
-const cash = new CashAmount(10.5);
-cash.addDoubleAmount(29.33);
-cash.toDouble(); // -> 39.83
 
-const cash = new CashAmount(10.5);
-cash.addDoubleAmount(29.33);
-cash.toDoubleString(); // -> '39.83'
+// const cash = new CashAmount(10.5);
+// cash.addDoubleAmount(29.33);
+// cash.toDoubleString(); // -> '39.83'
--- testsuite.js
+++ testsuite.js
@@ -3,3 +3,19 @@ describe("Always True", () => {
     expect(true).to.be.true;
   });
 });
+
+describe("expect the truth", () => {
+  it("add double amount", () => {
+    var cash = new Cash(10.5);
+    cash.addDoubleAmount(29.33);
+    expect(cash.toDouble()).to.eql(39.83);
+  });
+});
+
+describe("expect the truth", () => {
+  it("change to string", () => {
+    var cash = new Cash(10.5);
+    cash.addDoubleAmount(29.33);
+    expect(cash.toDoubleString()).to.eql("39.83");
+  });
+});
